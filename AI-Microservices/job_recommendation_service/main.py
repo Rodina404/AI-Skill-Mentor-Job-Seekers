ca471ea8ee467df6baab6bc289daa4c550a90641
Rodina404 <rodinamo2003@gmail.com>
2026-06-07 19:38:28 +0300

    fix: port 8007

--- AI-Microservices/job_recommendation_service/main.py
+++ AI-Microservices/job_recommendation_service/main.py
@@ -57,6 +57,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", "8004")),
+        port=int(os.getenv("PORT", "8007")),
         reload=os.getenv("ENV", "production") == "development"
     )
